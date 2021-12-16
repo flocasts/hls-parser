97cbc1cf8c9ee216c7ae13df6cb391b39006933c
Harold Waters <harold.waters@flosports.tv>
2021-12-16 12:23:40 -0600

    index.ts first pass

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+/*! Copyright Kuu Miyazaki. SPDX-License-Identifier: MIT */
+export { getOptions, setOptions } from './utils';
+export * as  parse from './parse';
+export * as  stringify from './stringify';
+export * as  types from './types';
