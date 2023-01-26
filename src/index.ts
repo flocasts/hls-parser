1a6e5ff93f27fe83f264d189084746b85275fe36
Andrew Bird <andrew.bird@flosports.tv>
2023-01-26 13:22:02 -0600

    Exposing the parse params

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 /*! Copyright Kuu Miyazaki. SPDX-License-Identifier: MIT */
 export { getOptions, setOptions } from './utils';
-export { parse } from './parse';
+export { parse, UserParseParams, ParseParams } from './parse';
 export { stringify } from './stringify';
 export * as types from './types';
 export * from './types';
